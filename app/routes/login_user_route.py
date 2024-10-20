36dcb4d5a10bc560262b30ecd8025ed29a18da3c
Sanjay <bangalorerenukapra.s@northeastern.edu>
2024-10-20 18:26:07 -0400

    added change

--- app/routes/login_user_route.py
+++ app/routes/login_user_route.py
@@ -3,7 +3,7 @@ from app.controllers.login_user_controller import auth,get_user_details, update_
 
 auth_bp = Blueprint("login_user", __name__)
 
-auth_bp.route('/v2/user/self', methods=['GET'])(auth.login_required(get_user_details))
-auth_bp.route('/v2/user/self', methods=['PUT'])(auth.login_required(update_user_details))
+auth_bp.route('/v1/user/self', methods=['GET'])(auth.login_required(get_user_details))
+auth_bp.route('/v1/user/self', methods=['PUT'])(auth.login_required(update_user_details))
 
 
